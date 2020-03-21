d602e6b3c9b9d02fec2d213b0c9d9be3823303bd
Michael <michael.ohara29@mail.dcu.ie>
2020-03-21 17:44:49 +0000

    Generate Storage List

--- Storage.java
+++ Storage.java
@@ -7,7 +7,23 @@ public class Storage{
 	private List<Part> StorageList;
 
 	public Storage(){
+		this.Storage = GenerateStorageList();
 
+
+	}
+
+	public List<Part> GenerateStorageList(){
+		List<Part> storageList = new ArrayList<Part>();
+		
+		for(int i = 0; i <= 3; i++ )
+		{
+			for(int j = 0;j<=2; j++)
+			{
+				Part newPart = new Part(i); // picks random part
+				storageList.add(newPart);
+			}
+		}
+		return storageList;
 
 	}
 
